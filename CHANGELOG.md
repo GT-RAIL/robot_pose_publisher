8783e1cf84c7a7ade1dce0ede806aea9b1f84d54
Russell Toris <rctoris@wpi.edu>
2014-04-03 10:08:05 -0400

    Dry branch cleanup

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+2014-04-03 - **0.3.0**
+ * Dry branch cleanup  [(rctoris)](https://github.com/rctoris/)
--- src/robot_pose_publisher.cpp
+++ src/robot_pose_publisher.cpp
@@ -1,40 +1,3 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2012, Worcester Polytechnic Institute
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the Worcester Polytechnic Institute nor the
- *     names of its contributors may be used to endorse or promote
- *     products derived from this software without specific prior
- *     written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- *********************************************************************/
-
 /*!
  * \file robot_pose_publisher.cpp
  * \brief Publishes the robot's position in a geometry_msgs/Pose message.
@@ -43,9 +6,10 @@
  * difference between /map and /base_link.
  *
  * \author Russell Toris, WPI - rctoris@wpi.edu
- * \date Sept 12, 2012
+ * \date April 3, 2014
  */
 
+#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Pose.h>
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
@@ -62,38 +26,59 @@ int main(int argc, char ** argv)
   // initialize ROS and the node
   ros::init(argc, argv, "robot_pose_publisher");
   ros::NodeHandle nh;
+  ros::NodeHandle nh_priv("~");
 
-  ros::Publisher pose_pub = nh.advertise<geometry_msgs::Pose>("/robot_pose", 1);
+  // configuring parameters
+  std::string map_frame, base_frame;
+  double publish_frequency;
+  bool is_stamped;
+  ros::Publisher p_pub;
+
+  nh_priv.param<std::string>("map_frame",map_frame,"/map");
+  nh_priv.param<std::string>("base_frame",base_frame,"/base_link");
+  nh_priv.param<double>("publish_frequency",publish_frequency,10);
+  nh_priv.param<bool>("is_stamped", is_stamped, false);
+
+  if(is_stamped)
+    p_pub = nh.advertise<geometry_msgs::PoseStamped>("robot_pose", 1);
+  else 
+    p_pub = nh.advertise<geometry_msgs::Pose>("robot_pose", 1);
 
   // create the listener
   tf::TransformListener listener;
-  listener.waitForTransform("/map", "/base_link", ros::Time(), ros::Duration(1.0));
+  listener.waitForTransform(map_frame, base_frame, ros::Time(), ros::Duration(1.0));
 
-  ros::Rate rate(10.0);
+  ros::Rate rate(publish_frequency);
   while (nh.ok())
   {
     tf::StampedTransform transform;
     try
     {
-      listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
+      listener.lookupTransform(map_frame, base_frame, ros::Time(0), transform);
+
+      // construct a pose message
+      geometry_msgs::PoseStamped pose_stamped;
+      pose_stamped.header.frame_id = map_frame;
+      pose_stamped.header.stamp = ros::Time::now();
+
+      pose_stamped.pose.orientation.x = transform.getRotation().getX();
+      pose_stamped.pose.orientation.y = transform.getRotation().getY();
+      pose_stamped.pose.orientation.z = transform.getRotation().getZ();
+      pose_stamped.pose.orientation.w = transform.getRotation().getW();
+
+      pose_stamped.pose.position.x = transform.getOrigin().getX();
+      pose_stamped.pose.position.y = transform.getOrigin().getY();
+      pose_stamped.pose.position.z = transform.getOrigin().getZ();
+
+      if(is_stamped)
+        p_pub.publish(pose_stamped);
+      else
+        p_pub.publish(pose_stamped.pose);
     }
     catch (tf::TransformException &ex)
     {
-      ROS_ERROR("%s", ex.what());
+      // just continue on
     }
-
-    // construct a pose message
-    geometry_msgs::Pose pose;
-    pose.orientation.x = transform.getRotation().getX();
-    pose.orientation.y = transform.getRotation().getY();
-    pose.orientation.z = transform.getRotation().getZ();
-    pose.orientation.w = transform.getRotation().getW();
-
-    pose.position.x = transform.getOrigin().getX();
-    pose.position.y = transform.getOrigin().getY();
-    pose.position.z = transform.getOrigin().getZ();
-
-    pose_pub.publish(pose);
 
     rate.sleep();
   }
